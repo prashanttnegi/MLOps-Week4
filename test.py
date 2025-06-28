075f084d104ed3d4ed114a31129459f20c47e5d0
prashanttnegi <prashant.negi0407@gmail.com>
2025-06-28 15:36:39 +0000

    First upload of all the required files

---
+++ test.py
@@ -0,0 +1,30 @@
+import unittest
+from joblib import load
+import pandas as pd
+
+class TestModel(unittest.TestCase):
+    model=None
+    model_path="artifacts/model.joblib"
+    ds= pd.read_csv('data/iris.csv')
+    
+    def setUp(self):
+        self.model=load(self.model_path)
+        
+    def test_data_validation(self):
+        print(f"## Running test for Data Validation")
+        # Ensure no missing values
+        self.assertFalse(self.ds.drop(columns='species').isnull().values.any(), "Missing values in features")
+        self.assertFalse(self.ds['species'].isnull().values.any(), "Missing values in labels")
+
+        # Ensure correct number of features
+        self.assertEqual(self.ds.drop(columns='species').shape[1], 4, "Expected 4 features")
+        
+    def test_evaluation(self):
+        print(f"## Running test for Model Evaluation")
+        sample = self.ds.drop(columns='species').iloc[0:1]
+        result = self.model.predict(sample)
+        expected = self.ds['species'].iloc[0]
+        self.assertEqual(result[0], expected, "Wrong prediction")
+            
+if __name__=="__main__":
+    unittest.main()
---
+++ train.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn import metrics
+
+data = pd.read_csv('data/iris.csv')
+
+train, test = train_test_split(data, test_size = 0.4, stratify = data['species'], random_state = 42)
+X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
+y_train = train.species
+X_test = test[['sepal_length','sepal_width','petal_length','petal_width']]
+y_test = test.species
+
+model = DecisionTreeClassifier(max_depth = 3, random_state = 1)
+model.fit(X_train,y_train)
+
+prediction=model.predict(X_test)
+
+# Compute confusion matrix
+labels = sorted(y_test.unique())
+cm = confusion_matrix(y_test, y_pred, labels=labels)
+
+# Compute confusion matrix
+labels = sorted(y_test.unique())
+cm = confusion_matrix(y_test, y_pred, labels=labels)
+
+# Convert to DataFrame for pretty printing
+cm_df = pd.DataFrame(cm, index=labels, columns=labels)
+
+# Print report for CML
+print(f"## Sanity Test Report")
+print(f"**Model Accuracy:** {metrics.accuracy_score(prediction,y_test):.3f}%\n")
+print("### Confusion Matrix (True vs Predicted):\n")
+print(cm_df.to_markdown())
+
+joblib.dump(model, "artifacts/model.joblib")
